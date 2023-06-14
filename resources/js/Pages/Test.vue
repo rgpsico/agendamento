354410abe7bb6be751180eb131ba1044912575b4
roger <rogernevesn@gmail.com>
2023-06-14 01:38:19 -0300

    auqi

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>{{ variavel }}</h1>
+      <p>Isto é uma página de teste.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        variavel: 'TESTE',
+      }
+    },
+  }
+  </script>
+  
+  <style scoped>
+  /* E aqui você pode adicionar seus estilos */
+  </style>
+  
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
+use Inertia\Inertia;
 
 Route::prefix('admin')->middleware(['check_user_authenticated'])->group(function () {
+});
+
+
+Route::get('/test', function () {
+    return Inertia::render('Test');
 });
