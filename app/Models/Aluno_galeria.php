d3755f7800739ac8e9b3c9b00f25c824b85c937c
roger <rogernevesn@gmail.com>
2023-07-08 10:52:54 -0300

    ajuste

--- app/Models/Aluno_galeria.php
+++ app/Models/Aluno_galeria.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Aluno_galeria extends Model
+class Aluno_Galeria extends Model
 {
     use HasFactory;
     public $timestamps = false;
