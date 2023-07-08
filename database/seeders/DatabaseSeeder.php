d7bd0fb6880e9663461c778c1503eb33f35e3278
roger <rogernevesn@gmail.com>
2023-07-08 10:58:02 -0300

    ajuste

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
 
         // \App\Models\Aulas::factory(10)->create();
         \App\Models\Professor::factory(10)->create();
-        \App\Models\Aluno_Galeria::factory(10)->create();
+        //\App\Models\Aluno_Galeria::factory(10)->create();
         \App\Models\Empresa::factory(10)->create();
         \App\Models\Servicos::factory(10)->create();
         \App\Models\Modalidade::factory(10)->create();
