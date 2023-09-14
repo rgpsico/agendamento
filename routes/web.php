3e9472523ca0d54dbac94b4c1ae782e85656d6ab
roger <rogernevesn@gmail.com>
2023-09-14 20:20:10 -0300

    ajuste

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AgendaController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SocialLiteController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
