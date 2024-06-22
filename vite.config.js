bc347333b61fba64e888a12798e976d43557699e
roger <rogernevesn@gmail.com>
2024-06-22 19:12:18 -0300

    AJUSTES

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+      
         laravel({
             input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
