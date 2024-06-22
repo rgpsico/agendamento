3656e0e877bbede6b5d5542bebc2d489cba69634
roger <rogernevesn@gmail.com>
2024-06-22 19:25:59 -0300

    AJUSTES

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,20 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/js/app.js', 'resources/css/app.css'],
+      input: ["resources/css/app.css", "resources/js/app.js"],
       refresh: true,
     }),
-  ],
-  build: {
-    outDir: 'public/build',
-    manifest: true,
-    rollupOptions: {
-      input: {
-        main: '/resources/js/app.js',
-        style: '/resources/css/app.css',
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
       },
-    },
-  },
-})
+    }),
+  ],
+});
