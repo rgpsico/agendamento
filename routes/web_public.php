bbfcd456ad3b3dc01b68b0e1bc34e99e6b3f3dd7
roger <rogernevesn@gmail.com>
2025-04-30 12:05:35 -0300

    ajustes

--- routes/web_public.php
+++ routes/web_public.php
@@ -9,9 +9,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::domain('roger.rjpasseios.com.br')->group(function () {
-    Route::get('/', [SiteController::class, 'mostrarDominio']);
-});
+Route::get('/', [SiteController::class, 'mostrarDominio']);
+
+
 
 
 Route::get('/registrar_escola', [HomeController::class, 'registerProf'])->name('public.registerProf');
@@ -38,7 +38,7 @@ Route::get('/registerAluno', [HomeController::class, 'registerAluno'])->name('ho
 
 Route::get('/login', [HomeController::class, 'login'])->name('home.login');
 
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
+// Route::get('/', [HomeController::class, 'index'])->name('home.index');
 Route::get('/{id}/empresa', [HomeController::class, 'show'])->name('home.show');
 Route::get('/{id}/bokking', [HomeController::class, 'booking'])->name('home.booking');
 Route::get('/{id}/checkout', [HomeController::class, 'checkout'])->name('home.checkout');
