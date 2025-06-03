67483ff0f3168b5547e34692b6d14ad8927298d3
roger <rogernevesn@gmail.com>
2025-06-03 19:56:36 -0300

    Ajuste pagamento Cartao de credito

--- resources/views/admin/empresas/index.blade.php
+++ resources/views/admin/empresas/index.blade.php
@@ -226,6 +226,22 @@
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="card">
+                            @if (session('success'))
+                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                    {{ session('success') }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                                </div>
+                            @endif
+
+                            <!-- Exibir mensagem de erro -->
+                            @if (session('error'))
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    {{ session('error') }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                                </div>
+                            @endif
                             <div class="card-body">
                                 <div class="table-responsive">
                                     <table class="datatable table table-hover table-center mb-0">
