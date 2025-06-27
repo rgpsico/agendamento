c3399656ec0d6977a85ae86d1aef39776b83cd57
roger <rogernevesn@gmail.com>
2025-06-27 20:24:47 -0300

    footer ref

--- resources/views/components/public/footer.blade.php
+++ resources/views/components/public/footer.blade.php
@@ -357,12 +357,12 @@
                     <div class="footer-widget footer-about">
                         <div class="footer-logo">
                             <a href="/">
-                                <img src="https://rjpasseios.com.br/wp-content/uploads/2024/12/cropped-logo-1.png"
-                                    alt="Logo RJ Passeios">
+                                <img src="{{ asset('admin/img/logorjpasseioedi.png') }}" alt="Logo RJ Passeios">
                             </a>
                         </div>
-                        <div class="footer-about-content">
-                            <p>Estamos comprometidos em promover a paixão pelos esportes ao ar livre, facilitando o
+                        <div class="footer-about-content" style="color:#fff;">
+                            <p style="color:#fff;">Estamos comprometidos em promover a paixão pelos esportes ao ar
+                                livre, facilitando o
                                 acesso, o engajamento e o sucesso dos atletas e comunidades costeiras em todo o mundo.
                             </p>
                         </div>
@@ -372,11 +372,11 @@
                 <!-- Menu Alunos -->
                 <div class="col-lg-2 col-md-4">
                     <div class="footer-widget footer-menu">
-                        <h2 class="footer-title">Alunos</h2>
+                        <h2 class="footer-title light" style="color:#fff;">Alunos</h2>
                         <ul>
-                            <li><a href="">Buscar Professores</a></li>
-                            <li><a href="{{ route('home.login') }}">Login</a></li>
-                            <li><a href="{{ route('home.registerAluno') }}">Registrar</a></li>
+                            <li><a style="color:#fff;" href="">Buscar Professores</a></li>
+                            <li><a style="color:#fff;" href="{{ route('home.login') }}">Login</a></li>
+                            <li><a style="color:#fff;" href="{{ route('home.registerAluno') }}">Registrar</a></li>
                         </ul>
                     </div>
                 </div>
@@ -384,11 +384,12 @@
                 <!-- Menu Professores -->
                 <div class="col-lg-2 col-md-4">
                     <div class="footer-widget footer-menu">
-                        <h2 class="footer-title">Professores</h2>
+                        <h2 class="footer-title" style="color:#fff;">Professores</h2>
                         <ul>
-                            <li><a href="{{ route('home.registerProf') }}">Cadastrar</a></li>
-                            <li><a href="https://wa.me/5521990271287" target="_blank" rel="noopener">WhatsApp</a></li>
-                            <li><a href="{{ route('home.login') }}">Login</a></li>
+                            <li><a style="color:#fff;" href="{{ route('home.registerProf') }}">Cadastrar</a></li>
+                            <li><a style="color:#fff;" href="https://wa.me/5521990271287" target="_blank"
+                                    rel="noopener">WhatsApp</a></li>
+                            <li><a style="color:#fff;"href="{{ route('home.login') }}">Login</a></li>
                         </ul>
                     </div>
                 </div>
@@ -396,7 +397,7 @@
                 <!-- Contato -->
                 <div class="col-lg-2 col-md-5">
                     <div class="footer-widget footer-contact">
-                        <h2 class="footer-title">Ligue pra gente</h2>
+                        <h2 class="footer-title" style="color:#fff;">Ligue pra gente</h2>
                         <div class="footer-contact-info">
                             <p><i class="feather-map-pin"></i> Rua Saint Roman, 200 – Rio de Janeiro, Brasil</p>
                             <p><i class="feather-phone-call"></i> +55 21 99027-1287</p>
@@ -408,7 +409,7 @@
                 <!-- Newsletter e Social -->
                 <div class="col-lg-3 col-md-7">
                     <div class="footer-widget">
-                        <h2 class="footer-title">Envie um e-mail</h2>
+                        <h2 class="footer-title" style="color:#fff;">Envie um e-mail</h2>
                         <form action="#" class="subscribe-form">
                             <input type="email" class="form-control" placeholder="Digite seu e-mail">
                             <button type="submit" class="btn">Enviar</button>
@@ -416,12 +417,12 @@
                         <div class="social-icon mt-3">
                             <ul>
                                 <li><a href="https://facebook.com" target="_blank" rel="noopener"><i
-                                            class="fab fa-facebook"></i></a></li>
-                                <li><a href="https://instagram.com" target="_blank" rel="noopener"><i
-                                            class="fab fa-instagram"></i></a></li>
-                                <li><a href="https://twitter.com" target="_blank" rel="noopener"><i
-                                            class="fab fa-twitter"></i></a></li>
-                                <li><a href="https://linkedin.com" target="_blank" rel="noopener"><i
+                                            class="fab fa-facebook" style="color:#fff;"></i></a></li>
+                                <li><a href="https://instagram.com" style="color:#fff;" target="_blank"
+                                        rel="noopener"><i class="fab fa-instagram " style="color:#fff;"></i></a></li>
+                                <li><a href="https://twitter.com" target="_blank" style="color:#fff;" rel="noopener"><i
+                                            class="fab fa-twitter" style="color:#fff;"></i></a></li>
+                                <li><a href="https://linkedin.com" target="_blank" rel="noopener" style="color:#fff;"><i
                                             class="fab fa-linkedin-in"></i></a></li>
                             </ul>
                         </div>
