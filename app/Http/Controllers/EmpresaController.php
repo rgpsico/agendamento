a1d5e7275ec83eff42db81ac79fbb65157975fd4
rgpsico <gerro121@hotmail.com>
2025-08-21 02:35:32 -0300

    JUSTES tela de empresas

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -305,7 +305,7 @@ class EmpresaController extends Controller
                 'descricao' => $validatedData['descricao'],
                 'telefone' => $validatedData['telefone'],
                 'cnpj' => $validatedData['cnpj'],
-                'data_vencimento' => $validatedData['data_vencimento'],
+                'data_vencimento' => $validatedData['data_vencimento'] ?? '',
                 'valor_aula_de' => $validatedData['valor_aula_de'],
                 'valor_aula_ate' => $validatedData['valor_aula_ate'],
                 'modalidade_id' => $validatedData['modalidade_id'],
