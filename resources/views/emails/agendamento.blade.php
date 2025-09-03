702ec800fd6e731b11affd43e6d2f33d9bdf4d99
rgpsico <gerro121@hotmail.com>
2025-09-03 13:08:08 -0300

    Ajustando o componente aluno

--- resources/views/emails/agendamento.blade.php
+++ resources/views/emails/agendamento.blade.php
@@ -5,9 +5,10 @@
     <title>Confirmação de Agendamento</title>
 </head>
 <body>
-    <h2>Olá, {{ $dados['cliente'] }}</h2>
+    <h2>Olá, {{ $dados['aluno'] }}</h2>
     <p>Seu agendamento foi confirmado!</p>
-    <p><b>Serviço:</b> {{ $dados['servico'] }}</p>
+    <p><b>Serviço:</b> {{ $dados['modalidade'] }}</p>
     <p><b>Data:</b> {{ $dados['data'] }}</p>
+
 </body>
 </html>
