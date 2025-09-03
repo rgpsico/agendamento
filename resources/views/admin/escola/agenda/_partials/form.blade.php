eec996d303737ef4a8f2a962cbc47b75a912f531
rgpsico <gerro121@hotmail.com>
2025-09-02 22:52:47 -0300

    Ajustando o componente aluno

--- resources/views/admin/escola/agenda/_partials/form.blade.php
+++ resources/views/admin/escola/agenda/_partials/form.blade.php
@@ -37,18 +37,7 @@
 </div>
 
     
-<div class="form-group">
-    <label for="selectModalidades">Selecione uma modalidade:</label>
-    <select id="selectModalidades" class="form-control" name="modalidade_id">
-        <option value="">Selecione</option>
-        @foreach ($modalidades as $modalidade)
-            <option value="{{ $modalidade->id }}" 
-                {{ old('modalidade_id', $selectedModalidade) == $modalidade->id ? 'selected' : '' }}>
-                {{ $modalidade->nome }}
-            </option>
-        @endforeach
-    </select>
-</div>
+
 
 
     
