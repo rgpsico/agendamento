37d9f8895b613b0cd184db3229f39a2440e786c0
rgpsico <gerro121@hotmail.com>
2025-09-08 15:27:13 -0300

    AJUSTES VH

--- resources/views/admin/virtualhosts/edit.blade.php
+++ resources/views/admin/virtualhosts/edit.blade.php
@@ -1,13 +1,16 @@
 <x-admin.layout title="Editar Virtual Host">
     <div class="page-wrapper">
         <div class="content container-fluid">
+
             <div class="page-header">
                 <div class="row">
-                    <div class="col-sm-6">
+                    <div class="col-sm-12">
                         <h3 class="page-title">Editar Virtual Host</h3>
                         <ul class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{ route('virtualhosts.index') }}">Virtual Hosts</a></li>
-                            <li class="breadcrumb-item active">Editar</li>
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('virtualhosts.index') }}">Virtual Hosts</a>
+                            </li>
+                            <li class="breadcrumb-item active">{{ $fileName }}</li>
                         </ul>
                     </div>
                 </div>
@@ -19,38 +22,27 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
+
                             <form action="{{ route('virtualhosts.update', $fileName) }}" method="POST">
                                 @csrf
                                 @method('PUT')
 
-                                <div class="mb-3">
-                                    <label for="servername" class="form-label">ServerName</label>
-                                    <input type="text" class="form-control" id="servername" name="servername" value="{{ $vhost['servername'] ?? '' }}" required>
+                                <div class="form-group">
+                                    <label for="content">Conteúdo do Virtual Host</label>
+                                    <textarea name="content" id="content" rows="20" class="form-control">{{ $content }}</textarea>
                                 </div>
 
-                                <div class="mb-3">
-                                    <label for="serveralias" class="form-label">ServerAlias</label>
-                                    <input type="text" class="form-control" id="serveralias" name="serveralias" value="{{ $vhost['serveralias'] ?? '' }}">
+                                <div class="mt-3 text-end">
+                                    <button type="submit" class="btn btn-primary">Salvar Alterações</button>
+                                    <a href="{{ route('virtualhosts.index') }}" class="btn btn-secondary">Cancelar</a>
                                 </div>
-
-                                <div class="mb-3">
-                                    <label for="documentroot" class="form-label">DocumentRoot</label>
-                                    <input type="text" class="form-control" id="documentroot" name="documentroot" value="{{ $vhost['documentroot'] ?? '' }}" required>
-                                </div>
-
-                                <div class="mb-3 form-check">
-                                    <input type="checkbox" class="form-check-input" id="enable_ssl" name="enable_ssl" 
-                                        {{ !empty($vhost['enable_ssl']) ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="enable_ssl">Habilitar SSL</label>
-                                </div>
-
-                                <button type="submit" class="btn btn-primary">Atualizar</button>
-                                <a href="{{ route('virtualhosts.index') }}" class="btn btn-secondary">Cancelar</a>
                             </form>
+
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 </x-admin.layout>
