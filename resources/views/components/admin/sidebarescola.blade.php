53b3167cbad9c2b173dbb783fb6a8036f3e7d193
rgpsico <gerro121@hotmail.com>
2025-09-08 13:11:02 -0300

    AJUSTES VH

--- resources/views/components/admin/sidebarescola.blade.php
+++ resources/views/components/admin/sidebarescola.blade.php
@@ -1,6 +1,6 @@
 <head>
     <!-- Outros links e scripts -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <style>
