138835e30ba717e1e6a51efcb5339c9607c4253f
rgpsico <gerro121@hotmail.com>
2025-09-08 13:39:51 -0300

    AJUSTES VH

--- app/Http/Requests/StoreSiteRequest.php
+++ app/Http/Requests/StoreSiteRequest.php
@@ -14,6 +14,7 @@ class StoreSiteRequest extends FormRequest
 
     public function rules(): array
     {
+        dd($this->all());
         return [
             'template_id' => 'required|exists:site_templates,id',
             'titulo' => 'required|string|max:255',
