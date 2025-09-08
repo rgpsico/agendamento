a41867783fa389c11a50463fe19beec58e440eba
rgpsico <gerro121@hotmail.com>
2025-09-08 13:09:55 -0300

    AJUSTES VH

--- resources/views/components/admin/sidebarescola.blade.php
+++ resources/views/components/admin/sidebarescola.blade.php
@@ -1,12 +1,8 @@
 <head>
     <!-- Outros links e scripts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <style>
         .sidebar-disabled {
             pointer-events: none;
