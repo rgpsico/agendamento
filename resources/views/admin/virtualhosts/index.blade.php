d6ce1f3c3a9640fbb0c5eff4789e66326e98f74d
rgpsico <gerro121@hotmail.com>
2025-09-08 15:16:10 -0300

    AJUSTES VH

--- resources/views/admin/virtualhosts/index.blade.php
+++ resources/views/admin/virtualhosts/index.blade.php
@@ -1,13 +1,27 @@
 <x-admin.layout title="Virtual Hosts">
+    <x-modal-delete />
+    <x-modal-editar-vhost />
     <div class="page-wrapper">
         <div class="content container-fluid">
+
             <div class="page-header">
                 <div class="row">
                     <div class="col-sm-6">
-                        <h3 class="page-title">Virtual Hosts</h3>
+                        <h3 class="page-title">Gerenciar Virtual Hosts</h3>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="">Admin</a></li>
+                            <li class="breadcrumb-item active">Virtual Hosts</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 text-end">
+                        <a href="{{ route('virtualhosts.create') }}" class="btn btn-success">
+                            <i class="fe fe-plus"></i> Criar Virtual Host
+                        </a>
                     </div>
                 </div>
             </div>
+
+            <x-alert-messages />
 
             <div class="row">
                 <div class="col-12">
@@ -18,17 +32,26 @@
                                     <thead>
                                         <tr>
                                             <th>Arquivo</th>
+                                            <th>ServerName</th>
                                             <th class="text-center">Ação</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @foreach ($files as $file)
+                                        @foreach ($vhosts as $vhost)
                                             <tr class="linha_{{ $loop->index }}">
-                                                <td>{{ $file->getFilename() }}</td>
+                                                <td>{{ $vhost['file'] }}</td>
+                                                <td>{{ $vhost['servername'] }}</td>
                                                 <td class="text-center">
-                                                    <button class="btn btn-sm bg-danger-light bt_excluir" data-file="{{ $file->getFilename() }}">
-                                                        <i class="fe fe-trash"></i> Excluir
-                                                    </button>
+                                                    <div class="actions">
+                                                        <a class="btn btn-sm bg-info-light bt_editar"
+                                                           data-file="{{ $vhost['file'] }}">
+                                                            <i class="fe fe-pencil"></i> Editar
+                                                        </a>
+                                                        <a class="btn btn-sm bg-danger-light bt_excluir"
+                                                           data-file="{{ $vhost['file'] }}">
+                                                            <i class="fe fe-trash"></i> Excluir
+                                                        </a>
+                                                    </div>
                                                 </td>
                                             </tr>
                                         @endforeach
@@ -47,36 +70,36 @@
 <!-- DataTables -->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-<!-- GSAP -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/gsap.min.js"></script>
 
 <script>
 $(document).ready(function() {
-  
+    $(".datatable").DataTable();
 
+    // Excluir
     $(document).on("click", ".bt_excluir", function() {
-        let file = $(this).data('file');
-        let row = $(this).closest('tr');
-
-        if(confirm('Deseja realmente excluir o Virtual Host "' + file + '"?')) {
-       $.ajax({
-            url: '{{ route("virtualhosts.destroy", ":file") }}'.replace(':file', file),
-            type: 'POST',
-            data: {
-                _method: 'DELETE',
-                _token: $('meta[name="csrf-token"]').attr('content')
-            },
-            success: function(res) {
-                gsap.to(row, { opacity: 0, y: -20, duration: 0.5, onComplete: () => row.remove() });
-            }
-        });
-
+        var file = $(this).data('file');
+        if(confirm('Deseja excluir o vhost ' + file + '?')) {
+            $.ajax({
+                url: '{{ route("virtualhosts.destroy", ":file") }}'.replace(':file', file),
+                type: 'DELETE',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                success: function(res) {
+                    alert('Vhost excluído com sucesso');
+                    location.reload();
+                },
+                error: function(err) {
+                    console.log(err);
+                }
+            });
         }
     });
 
-    // animação de entrada das linhas
-    $('tbody tr').each(function(i){
-        gsap.from($(this), {opacity:0, y:20, duration:0.5, delay:i*0.05});
+    // Editar
+    $(document).on("click", ".bt_editar", function() {
+        var file = $(this).data('file');
+        window.location.href = '{{ route("virtualhosts.edit", ":file") }}'.replace(':file', file);
     });
 });
 </script>
