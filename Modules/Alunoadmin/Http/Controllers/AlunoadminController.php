0fd3986070cada3551779b99f091499c76c07da9
Roger Neves <rogerneves@blumar.com.br>
2026-01-18 22:35:44 -0300

    Tela de login

--- Modules/Alunoadmin/Http/Controllers/AlunoadminController.php
+++ Modules/Alunoadmin/Http/Controllers/AlunoadminController.php
@@ -72,7 +72,7 @@ class AlunoadminController extends Controller
        
         $query = Agendamento::with('professor.usuario', 'modalidade')
             ->where('aluno_id', $id);
-            dd($request->all());
+            
 
         if ($request->filled('data')) {
             $query->whereDate('data_da_aula', $request->data);
