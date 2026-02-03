33126337652af3087847b60cb86de5c37fd9d62a
Roger Neves <rogerneves@blumar.com.br>
2026-02-03 17:02:19 -0300

    tela de deluxe

--- routes/api.php
+++ routes/api.php
@@ -360,6 +360,7 @@ Route::post('/bots/{bot}/chat', [BotController::class, 'chat']);
 Route::get('/conversations/aberta', [ChatProfessorController::class, 'verificarConversaAlunoEmpresa']);
 Route::get('/conversations', [ChatController::class, 'listByEmpresaAndUser']);
 Route::get('/conversations/lista', [ChatController::class, 'listByEmpresaOrProfessor']);
+Route::post('/conversations/last/opened', [ChatController::class, 'markLastConversationOpened']);
 Route::get('/conversations/{id}', [ConversationController::class, 'showapi'])->whereNumber('id');
 
 
