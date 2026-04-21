2571c3bd516199e909aa7d4c49033cc2fcbe1a59
Roger Neves <rogerneves@blumar.com.br>
2026-04-21 01:09:48 -0300

    Organizando pasta venues e hotel dentro da pasta pages

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+
+        // Horizon::night();
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     */
+    protected function gate(): void
+    {
+        Gate::define('viewHorizon', function ($user) {
+            return in_array($user->email, [
+                'rgyr2010@hotmail.com', // troca pelo seu email
+            ]);
+        });
+    }
+}
